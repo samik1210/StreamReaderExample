e59b584e43cfd63eecfaa705e482b83e664ccfe8
parag <parag@DESKTOP-I2Q9QKQ>
2021-09-01 13:22:11 +0530

    Add project files.

---
+++ StreamReaderExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace StreamReaderExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            FileStream f = new FileStream("c:\\Samu.txt", FileMode.OpenOrCreate);
+            StreamReader s = new StreamReader(f);
+
+            string line = "";
+            while ((line = s.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+            s.Close();
+            f.Close();
+        }
+    
+    }
+}
